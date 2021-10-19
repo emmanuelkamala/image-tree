d47f626feadd03cfbcf242b30d693ed791fd8b40
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-10-20 02:58:59 +0300

    Add styles to the app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import Canvas from './Canvas';
+import './app.css';
 
 function App() {
   return (
-    <Canvas />
+    <div className="home">
+      <button className="button"><h4>Hi</h4></button>
+    </div>
   );
 }
 
